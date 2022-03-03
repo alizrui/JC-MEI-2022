c8e6cc434118a304e00b32fa14e887c840d91315
Alejandro <alejandroiznardo@gmail.com>
2022-03-03 17:07:30 +0100

    oaoaoaoa

--- DrYoshi/code/scene.js
+++ DrYoshi/code/scene.js
@@ -5,11 +5,18 @@ const VIRUS_ANIMATION = 0;
 
 function Scene()
 {
-	// Load bottle texture
-	var bottleTexture = new Texture("../img/fondo_juego_v1.png");
+	// Load textures
+	var fondo_juego = new Texture("../img/fondo_juego_v1.png");
+	var menu_principal = new Texture("../img/menu_principal.png");
+	var fondo_creditos = new Texture("../img/fondo_creditos.png");
+	var instrucciones = new Texture("../img/instrucciones.png");
+		
+	// Create objects
+	this.fjuego = new StaticImage(0, 0, 512, 480, fondo_juego);
+	this.mprincipal = new StaticImage(0, 0, 512, 480, menu_principal);
+	this.fcreditos = new StaticImage(0, 0, 512, 480, fondo_creditos);
+	this.instr = new StaticImage(0, 0, 512, 480, instrucciones);
 	
-	// Create bottle Image object
-	this.bottle = new StaticImage(0, 0, 512, 480, bottleTexture);
 	
 	// Loading texture to use in a TileMap
 	var tilesheet = new Texture("../tiles/tiles16.png");
@@ -31,7 +38,7 @@ Scene.prototype.update = function(deltaTime)
 	// this.virusSprite1.update(deltaTime)
 }
 
-Scene.prototype.draw = function ()
+Scene.prototype.draw = function () // meter argumento
 {
 	// Get canvas object, then its context
 	var canvas = document.getElementById("game-layer");
@@ -41,8 +48,8 @@ Scene.prototype.draw = function ()
 	context.fillStyle = "rgb(224, 224, 240)";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	// Draw bottle
-	this.bottle.draw();
+	// Draw scenes (if else)
+	this.fondo_juego.draw();
 
 	// Draw tilemap
 	this.map.draw();
